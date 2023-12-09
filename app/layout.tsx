5a3d743e340a031b576cc6c173a39891ce3b9460
Filipe <filipe-all@hotmail.com>
2023-12-09 18:56:26 -0300

    Feat: Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
+import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
 import "./globals.css";
@@ -19,6 +20,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
+                <Navbar />
                 {children}
                 <Footer />
             </body>
