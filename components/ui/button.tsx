2a88a31a74acfe89d13be681c7971828dd0d561b
Filipe <filipe-all@hotmail.com>
2023-12-10 13:49:51 -0300

    Feat: Category Page
    With filter by colors and sizes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,6 +9,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         return (
             <button
                 ref={ref}
+                {...props}
                 className={cn(
                     "w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed text-white font-semibold hover:opacity-75 transition",
                     className
