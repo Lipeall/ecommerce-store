2ce5ac3529fb319ed471b23a4197550fce3e4eae
Filipe <filipe-all@hotmail.com>
2023-12-09 21:31:01 -0300

    Feat: Product list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,26 @@
-const HomePage = () => {
-    return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import ProductList from "@/components/ui/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard(
+        "ae6875a8-88fb-4236-b05f-30556c527913"
+    );
+    const products = await getProducts({ isFeatured: true });
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
+    );
 };
 
 export default HomePage;
