082d6e44d3eead6bb18dfaa9f721a2937a26e693
Filipe <filipe-all@hotmail.com>
2023-12-10 21:03:40 -0300

    Fix: Info

--- components/info.tsx
+++ components/info.tsx
@@ -4,11 +4,18 @@ import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product;
 }
 const Info = ({ data }: InfoProps) => {
+    const cart = useCart();
+
+    const onAddToCart = () => {
+        cart.addItem(data);
+    };
+
     return (
         <div>
             <h1 className="text-3xl font-bold text-grey-900">{data.name}</h1>
@@ -32,7 +39,10 @@ const Info = ({ data }: InfoProps) => {
                 </div>
             </div>
             <div className="mt-10 flex items-center gap-x-3">
-                <Button className="flex items-center gap-x-2">
+                <Button
+                    onClick={onAddToCart}
+                    className="flex items-center gap-x-2"
+                >
                     Add To Cart
                     <ShoppingCart />
                 </Button>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ export const revalidate = 0;
 
 const Navbar = async () => {
     const categories = await getCategories();
+    
     return (
         <div className="border-m">
             <Container>
